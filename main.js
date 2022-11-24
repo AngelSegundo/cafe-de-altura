4be9fd50fb104e1f8c883bdc1fdcdce09567510d
AngelSegundo <angel.segundo.dev@gmail.com>
2022-11-25 00:52:52 +0100

    Update question and answer dropdown

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//selectors
 
 let allProducts = document.querySelector(".productCardWrapper")
 let containerBuyCart = document.querySelector('.card-items')
@@ -9,6 +10,7 @@ let totalCard = 0
 let countProduct = 0
 
 //funtions
+
 loadEventListenrs()
 function loadEventListenrs() {
     allProducts.addEventListener('click', addProduct)
@@ -26,7 +28,6 @@ function addProduct(e) {
 function deleteProduct(e) {
     if (e.target.classList.contains("delete-product")) {
         const deleteId = e.target.getAttribute('data-id')
-
         shoppingCart.forEach(value => {
             if (value.id == deleteId) {
                 let priceReduce = parseFloat(value.price) * parseFloat(value.number)
@@ -38,7 +39,6 @@ function deleteProduct(e) {
         countProduct--
     }
     shoppingCartProducts()
-
 }
 
 
@@ -83,7 +83,7 @@ function shoppingCartProducts() {
             <img src="${image}" alt="">
             <div class="item-content">
                 <h5>${nameProduct}</h5>
-                <h5 class="cart-price">${price}$</h5>
+                <h5 class="cart-price">${price}€</h5>
                 <h6>Cantidad: ${number}</h6>
             </div>
             <span class="delete-product" data-id="${id}">X</span>
@@ -102,10 +102,87 @@ function clearHtml() {
     containerBuyCart.innerHTML = ''
 }
 
-
 function showCart(x) {
     document.getElementById("products-id").style.display = "block";
 }
 function closeBtn() {
     document.getElementById("products-id").style.display = "none";
 }
+
+
+//--- --- --- ---  Desplegable de Preguntas y Respuestas --- --- ---//
+
+const questions = document.querySelectorAll(".infoFAQ")
+const answer = document.querySelectorAll(".answer")
+const chevronup = document.querySelectorAll('.chevronup img')
+
+questions.forEach((question, key) => {
+    question.addEventListener("click", () => {
+        answer[key].classList.toggle("open_close")
+        rotate(chevronup[key])
+    })
+})
+
+const rotate = (img) => {
+    if (img.style.transform === '') {
+        img.style.transform = 'rotate(180deg)'
+        img.style.transition = "all 0.4s"        
+    } else {
+        img.style.transform = ''
+        img.style.transition = "all .7s"
+    }
+}
+
+// --- --- --- --- //
+//--- --- --- ---  Validacion de Formulario --- --- ---//
+
+let form = document.querySelector('#form')
+let nameInput = document.querySelector('#NameForm')
+let emailInput = document.getElementById('emailForm')
+let phoneInput = document.getElementById('phoneForm')
+let textArea = document.querySelector('#textArea1')
+let checker = document.getElementById('checker')
+
+const formValidation = (e) => {
+    e.preventDefault()
+    if (!nameInput.value) {
+        alert('Por favor, introduce tu nombre.')
+        nameInput.focus()
+        return
+    }
+
+    if (!emailInput.value.includes('@')) {
+        alert('«Introduzca una dirección de correo electrónico válida»\n\n(los datos que introdujiste no están en el formato correcto).')
+        emailInput.focus()
+        return
+    }
+
+    if (!(phoneInput.value * 1)) {
+        alert('Formato de TELEFONO incorrecto')
+        phoneInput.focus()
+        return
+    }
+
+    if (textArea.value == "") {
+        alert('\n¿En qué podemos ayudarte?\n\nEnvianos un comentario')
+        textArea.focus()
+        return
+    }
+
+    if (!checker.checked) {
+        let aceptar = confirm('Aceptas la Politica de Privacidad y los Terminos y condiciones?')
+        if (aceptar) {
+            checker.checked = true
+        } else {
+            alert('Es necesario aceptar.')
+        }
+        return
+    }
+
+    alert('El boton ENVIAR está funcionando')
+}
+
+form.addEventListener('submit', formValidation)
+
+// --- --- --- --- --- --- --- ---//
+
